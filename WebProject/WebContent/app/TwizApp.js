7775965ac80eac183eab0eff183d73ad0435e2dc
Koding <asif1924@asif1924.koding.io>
2016-02-20 15:00:53 -0600

    updated the app and corrected local.properties

--- WebProject/WebContent/app/TwizApp.js
+++ WebProject/WebContent/app/TwizApp.js
@@ -2,9 +2,36 @@ ensureNamespaceExists();
 
 HackathonWeb.TwizApp = function(){
     var logPrefix = "[HackathonWeb.TwizApp]:";
+    var $screenContainer = $("#MainPage");
+    
+    
     
     this.init = function(){
         var methodName = "init() ";
         console.log(logPrefix + methodName);
+        
+        createView();
+        bindEvents();
     }
+    
+    this.show = function(){
+        var methodName = "show() ";
+        console.log(logPrefix + methodName);
+        
+        
+    }
+    
+    function createView(){
+        var methodName = "createView() ";
+        console.log(logPrefix + methodName);
+        
+    }
+    
+    function bindEvents(){
+        var methodName = "bindEvents() ";
+        console.log(logPrefix + methodName);
+        
+    }
+    
+    
 }
