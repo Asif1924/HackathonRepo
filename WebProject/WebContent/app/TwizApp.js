22c6a1040c0723f838b1f040fed150edaaee7098
Koding <asif1924@asif1924.koding.io>
2016-02-20 17:20:23 -0600

    Added three.min.js

--- WebProject/WebContent/app/TwizApp.js
+++ WebProject/WebContent/app/TwizApp.js
@@ -4,6 +4,8 @@ HackathonWeb.TwizApp = function(){
     var logPrefix = "[HackathonWeb.TwizApp]:";
     var $screenContainer = $("#MainPage");
     
+    var renderWidth = 1024;
+    var renderHeight = 768;
     
     
     this.init = function(){
@@ -20,6 +22,40 @@ HackathonWeb.TwizApp = function(){
         
         $screenContainer.show();
         
+        start3D();
+    }
+    
+    function start3D(){
+			var scene = new THREE.Scene();
+			//var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+			
+			var camera = new THREE.PerspectiveCamera( 75, renderWidth/renderHeight, 0.1, 1000 );
+
+			var renderer = new THREE.WebGLRenderer();
+			//renderer.setSize( window.innerWidth, window.innerHeight );
+			renderer.setSize( renderWidth, renderHeight );
+
+			//document.body.appendChild( renderer.domElement );
+            $screenContainer.append( renderer.domElement );
+            
+            
+			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+			var render = function () {
+				requestAnimationFrame( render );
+
+				cube.rotation.x += 0.1;
+				cube.rotation.y += 0.1;
+
+				renderer.render(scene, camera);
+			};
+
+			render();        
     }
     
     function createView(){
